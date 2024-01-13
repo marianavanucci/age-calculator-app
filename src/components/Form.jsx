9a222ef1be1ce5b7367fd91d53cc4e477057e4dd
Mariana Vanucci <marianavanucciandrade@gmail.com>
2024-01-12 23:56:42 -0300

    Form.jsx  css

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,60 @@
 
 const Form = () => {
   return (
-    <form className="px-6" >
+    <form className=" py-12 pl-6 flex flex-col content-center items-center " >
         <div className="flex flex-row">
             <div className="w-1/3">
-                <label>DAY</label>
+                    <label className="text-xs
+                                text-slate-500
+                                font-semibold
+                                tracking-widest">YEAR</label>
+                    <input type="name" className="border-neutral-300
+                                                border-2
+                                                rounded-md
+                                                w-24
+                                                h-14
+                                                font-black
+                                                text-lg
+                                                pl-4"></input>
+                </div>
+            <div className="w-1/3">
+                <label className="text-xs
+                            text-slate-500
+                            font-semibold
+                            tracking-widest">MONTH</label>
                 <input type="name" className="border-neutral-300
                                                border-2
                                                rounded-md
-                                               w-8"></input>
+                                               w-24
+                                               h-14
+                                               font-black
+                                               text-lg
+                                               pl-4"></input>
             </div>
-            <div>
-                <label>MONTH</label>
-                <input type="name"></input>
-            </div>
-            <div>
-                <label>YEAR</label>
-                <input type="namer"></input>
+            <div className="w-1/3">
+                <label className="text-xs
+                            text-slate-500
+                            font-semibold
+                            tracking-widest">YEAR</label>
+                <input type="name" className="border-neutral-300
+                                               border-2
+                                               rounded-md
+                                               w-24
+                                               h-14
+                                               font-black
+                                               text-lg
+                                               pl-4"></input>
             </div>
         </div>
         <div>
-            <button type="submit">&#9660;</button>
+            <button type="submit" className="bg-violet-600
+                                            rounded-full
+                                            border-current
+                                            m-auto
+                                            text-white
+                                            w-14
+                                            h-14
+                                            my-8">&#9660;</button>
         </div>
     </form>
   )
